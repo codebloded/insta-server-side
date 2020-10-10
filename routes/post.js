cbe2364af5223a65d378a85186893f17430b5a5d
Rohan <rohanrajput16.phy@gmail.com>
2020-10-11 04:21:27 +0530

    Adding mypost createpost route

--- routes/post.js
+++ routes/post.js
@@ -4,6 +4,27 @@ const mongoose = require('mongoose');
 const Post = require('../models/Post');
 const router = express.Router();
 const authLogin = require('../middleware/authLogin');
+const { json } = require('body-parser');
+
+
+router.get('/allpost',async (req,res)=>{
+    try{
+        const posts = await Post.find().populate("postedBy","_id name");
+        res.json({posts:posts});
+    }catch(error){
+        res.json({err:error});
+    }
+})
+
+router.get('/mypost',authLogin, async(req,res)=>{
+    try{
+        const myPosts = await Post.find({postedBy:req.user._id}).populate('postedBy',"_id name");
+        res.json({myPosts});
+    }catch(error){
+        console.log(error);
+        res.status(404).json({error});
+    }
+});
 
 router.post("/createpost",authLogin, async (req, res)=>{
     req.header('Content-Type', 'application/json');
@@ -11,14 +32,14 @@ router.post("/createpost",authLogin, async (req, res)=>{
     if(!title || !body){
         return res.status(422).json({message:"Please add all feilds"});
     }
-        req.user.password = undefined;
-        const post = new Post({
+    req.user.password = undefined;
+    const post = new Post({
         title,
         body,
         postedBy:req.user
     })
     try{
-
+        
         const savedPost = await post.save();
         res.json({post:savedPost});
     }catch(err){
