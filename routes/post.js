6b5e7c737da968362689337b0cfc6167d9bd2d61
Rohan <rohanrajput16.phy@gmail.com>
2020-10-13 17:19:04 +0530

    adding like unlike comments routes and update the postand user models as well

--- routes/post.js
+++ routes/post.js
@@ -5,11 +5,14 @@ const Post = require('../models/Post');
 const router = express.Router();
 const authLogin = require('../middleware/authLogin');
 const { json } = require('body-parser');
+const User = require('../models/User');
 
 
 router.get('/allpost', async (req, res) => {
     try {
-        const posts = await Post.find().populate("postedBy", "_id name");
+        const posts = await Post.find()
+        .populate("postedBy", "_id name")
+        .populate("comments.postedBy", "_id name")
         res.json({ posts: posts });
     } catch (error) {
         res.json({ err: error });
@@ -48,6 +51,46 @@ router.post("/createpost", authLogin, async (req, res) => {
     }
 })
 
+router.put('/like',authLogin,(req,res)=>{
+    Post.findByIdAndUpdate(req.body.postId,{
+        $push:{likes:req.user._id},
+    },{new:true}).exec((err,result)=>{
+        if(err){
+            return res.status(422).json({error:err})
+        }else{
+            res.json(result);
+        }
+    })
+})
+
+router.put('/unlike',authLogin,(req,res)=>{
+    Post.findByIdAndUpdate(req.body.postId,{
+        $pull:{likes:req.user._id},
+    },{new:true}).exec((err,result)=>{
+        if(err){
+            return res.status(422).json({error:err})
+        }else{
+            res.json(result);
+        }
+    })
+});
+
+router.put('/comment',authLogin,(req,res)=>{
+        const comment = {
+            text:req.body.text,
+            postedBy:req.user._id
+        }
+        Post.findByIdAndUpdate(req.body.postId,{
+            $push:{comments:comment}
+        },{new:true}).populate("comments.postedBy", "id name")
+        .exec((err,result)=>{
+            if(err){
+                res.status(404).json({error:err});
+            }else{
+                res.json(result); 
+            }
+        })
+    })
 
 
 
